e7d9eb6cf355c817855d50c6121a361e9e0b4f87
KaJuan Johnson <kdjohnson23@oakland.edu>
2018-05-11 16:14:08 -0400

    Migration to Spring Boot 2 #22

--- src/main/java/edu/oakland/translations/TranslationsApplication.java
+++ src/main/java/edu/oakland/translations/TranslationsApplication.java
@@ -1,13 +1,12 @@
 package edu.oakland.translations;
 
+import org.springframework.beans.factory.FactoryBean;
+import org.springframework.beans.factory.config.ServiceLocatorFactoryBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
-
-import org.springframework.beans.factory.config.ServiceLocatorFactoryBean;
-import org.springframework.beans.factory.FactoryBean;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
